f09e3eb7075b89d6f00594c46a2d4dc3a767e62f
Tomas Mulder <tcmulder@gmail.com>
2025-10-22 12:10:32 -0600

    Refactor faux inner blocks

--- block-ui/fauxInnerBlocks.js
+++ block-ui/fauxInnerBlocks.js
@@ -7,45 +7,39 @@
  * in PHP. This ephemeral attribute isn't saved and is only used within the editor itself, not the front-end.
  *
  * @see https://wordpress.stackexchange.com/questions/385880/wp-blocks-gutenberg-serversiderender-not-rendering-content
+ * @version 1.0.0
  */
 
 import { select } from '@wordpress/data';
 import { getBlockContent } from '@wordpress/blocks';
 
 /**
- * Get block content to pass into block editor context.
+ * Show inner blocks within <ServerSideRender />
  *
  * Implementation steps:
+ * 
  * 1. In your block.json, add a `fauxInnerBlocks` attribute:
- *    ```json
- *    "attributes": {
- *      "fauxInnerBlocks": {
- *        "type": "string"
- *      }
- *    }
- *    ```
+ *   "attributes": {
+ *     "fauxInnerBlocks": {
+ *       "type": "string"
+ *     }
+ *   }
  *
  * 2. In your block component, call getFauxInnerBlocks() to retrieve the editor's inner blocks:
- *    ```js
- *    const { clientId } = props;
- *    const fauxInnerBlocks = getFauxInnerBlocks(clientId);
- *    ```
+ *   const { clientId } = props;
+ *   const fauxInnerBlocks = getFauxInnerBlocks(clientId);
  *
- * 3. Again in your block component, pass the content to ServerSideRender:
- *    ```js
- *    <ServerSideRender
- *      block="namespace/block-name"
- *      attributes={{
- *        ...attributes,
- *        fauxInnerBlocks,
- *      }}
- *    />
- *    ```
+ * 3. Again in your block component, pass the content to ServerSideRender as an attribute:
+ *   <ServerSideRender
+ *     block="namespace/block-name"
+ *     attributes={{
+ *       ...attributes,
+ *       fauxInnerBlocks,
+ *     }}
+ *   />
  *
  * 4. In your PHP render file, use the faux content if it exists (in the editor) or the normal $content if not (front-end):
- *    ```php
- *    $content = isset( $attributes['fauxInnerBlocks'] ) ? do_shortcode( do_blocks( $attributes['fauxInnerBlocks'] ) ) : $content;
- *    ```
+ *   $content = isset( $attributes['fauxInnerBlocks'] ) ? do_shortcode( do_blocks( $attributes['fauxInnerBlocks'] ) ) : $content;
  *
  * @param {string} clientId The client ID of the block from which to capture the inner blocks
  * @return {string}         The inner blocks content
